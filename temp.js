ac8839f71deaabd6dfd7c3a0996cfb539b194d45
Autumn Apsey <autumnapsey@gmail.com>
2016-04-15 15:52:30 -0400

    added conversion if else statement for F and C conversion

--- temp.js
+++ temp.js
@@ -1,4 +1,10 @@
 var temp = 212;
-var conversion = (temp - 32)*5/9;
-console.log(conversion);
-
+var farenheit = true
+if (farenheit){
+	var conversion = (temp - 32)*5/9;
+	console.log(conversion);
+	}
+	else {
+		var conversion = temp*9/5+32;
+		console.log(conversion);
+	}
