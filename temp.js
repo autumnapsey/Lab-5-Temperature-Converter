6461b2da5b6d266da32065b1ee6558d0aeb127e2
Autumn Apsey <autumnapsey@gmail.com>
2016-04-15 16:12:35 -0400

    added decriptive output to explain the conversion taking place

--- temp.js
+++ temp.js
@@ -1,10 +1,10 @@
-var temp = 212;
+var temp = 212
 var farenheit = true
 if (farenheit){
 	var conversion = (temp - 32)*5/9;
-	console.log(conversion);
+	console.log(temp + " degrees Farenheit converts to " + conversion + " degrees Celsius");
 	}
 	else {
 		var conversion = temp*9/5+32;
-		console.log(conversion);
+		console.log(temp + " degrees Celsius converts to " + conversion + " degrees Farenheit");
 	}
